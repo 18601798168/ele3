0b4f03a942f19cbf773f7abe707e455f76198863
刘毅 <lilugirl2005@126.com>
2020-09-09 11:34:56 +0800

    第二次修改

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+
+    fdafasfasdfdsafsaf
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
